a6e10f73a8e37a659796230299fc2ef24c45d9a0
bytestudioinc <53560022+bytestudioinc@users.noreply.github.com>
2025-09-28 14:51:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ io.on("connection", (socket) => {
   { 
     state: "ready",
     userId: socket.id,
-    version: "1.13",          // app version
+    version: "1.14",          // app version
     reward: 1,                // daily reward points
     preferenceCost: 10,        // cost for preference feature
     maintenance: "no",          // "yes" or "no" });
