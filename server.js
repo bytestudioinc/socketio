2abc25c8587559befeded446d753815263348eb6
bytestudioinc <53560022+bytestudioinc@users.noreply.github.com>
2025-09-29 01:15:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -269,7 +269,7 @@ io.on("connection", (socket) => {
       if (s) {
         sendToClient(s, "chat_response", {
           status: "chatting",
-          croomId: socket.roomId,
+          croomId: roomId,
           from: socket.id,
           name,
           gender,
